0256f8fd9ef3cdfbbd3cd1efe708961ffcb13fa7
olashina201 <olashina201@gmail.com>
2023-03-31 02:47:32 +0100

    new update

--- _printf.c
+++ _printf.c
@@ -1,51 +1,52 @@
 #include "main.h"
 
+/* Code Credit: Igwe Roseline & Paul Sabonde */
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - prints anything
+ * @format: the format string
  *
- * Return: number of chars printed.
+ * Return: number of bytes printed
  */
-
+/* Code Credit: Igwe Roseline & Paul Sabonde  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, buff_count = 0, prev_buff_count = 0;
-	char buffer[2000];
-	va_list arg;
-	call_t container[] = {
-		{'c', parse_char}, {'s', parse_str}, {'i', parse_int}, {'d', parse_int},
-		{'%', parse_perc}, {'b', parse_bin}, {'o', parse_oct}, {'x', parse_hex},
-		{'X', parse_X}, {'u', parse_uint}, {'R', parse_R13}, {'r', parse_rev},
-		{'\0', NULL}
-	};
+	int add = 0;
+	va_list aa;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-	if (!format)
+	va_start(aa, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	va_start(arg, format);
-	while (format && format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		if (format[i] == '%')
+		init_params(&params, aa);
+		if (*p != '%')
 		{
-			i++, prev_buff_count = buff_count;
-			for (j = 0; container[j].t != '\0'; j++)
-			{
-				if (format[i] == '\0')
-					break;
-				if (format[i] == container[j].t)
-				{
-					buff_count = container[j].f(buffer, arg, buff_count);
-					break;
-				}
-			}
-			if (buff_count == prev_buff_count && format[i])
-				i--, buffer[buff_count] = format[i], buff_count++;
+			add += _putchar(*p);
+			continue;
 		}
+		start = p;
+		p++;
+		while (get_flag(p, &params)) /* while char at p is flag char */
+		{
+			p++; /* next char */
+		}
+		p = get_width(p, &params, aa);
+		p = get_precision(p, &params, aa);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			add += print_from_to(start, p,
+								 params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			buffer[buff_count] = format[i], buff_count++;
-		i++;
+			add += get_print_func(p, aa, &params);
 	}
-	va_end(arg);
-	buffer[buff_count] = '\0';
-	print_buff(buffer, buff_count);
-	return (buff_count);
+	_putchar(BUF_FLUSH);
+	va_end(aa);
+	return (add);
 }
+/* Code Credit: Igwe Roseline & Paul Sabonde  */
